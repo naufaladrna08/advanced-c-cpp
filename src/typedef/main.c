f9c42894520eb8cb77db4e528c0cc717b71e778a
naufaladrna08 <naufaladrn90@gmail.com>
2022-01-31 13:29:55 +0700

    added typedef

---
+++ src/typedef/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef char* str;
+
+int main(int argc, char const *argv[]) {
+  str name = "Naufal Adriansyah";
+
+  printf("My name is %s\n", name);
+
+  return 0;
+}
